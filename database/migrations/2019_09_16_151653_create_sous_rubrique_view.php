f608751d4aae1a8c9b57138a3ee5a49de7117ede
BOUARFA Karim <BOUARFA@NOVEC.LAN>
2019-10-01 14:27:55 +0100

    Few changes

--- database/migrations/2019_09_16_151653_create_sous_rubrique_view.php
+++ database/migrations/2019_09_16_151653_create_sous_rubrique_view.php
@@ -13,11 +13,13 @@ class CreateSousRubriqueView extends Migration
      */
     public function up()
     {
-        DB::connection()->getPdo()->exec("CREATE VIEW taux_avancement_sous_rubrique as 
-        SELECT m.num_marche as 'marche', r.lib_rubrique as 'rubrique', sb.lib_sous_rubrique as 'sous_rubrique', p.date_attachement as 'periode', (c.valeur_constat / sb.valeur_cible) as 'taux_avancement_sous_rubrique' FROM 
-        periodes p inner join constats c on p.id_periode = c.id_periode
-        inner join sous_rubriques sb on c.id_sous_rubrique = sb.id_sous_rubrique
-        inner join rubriques r on r.id = sb.id_rubrique
+        DB::connection()->getPdo()->exec("create view taux_avancement_sous_rubrique as
+        SELECT m.num_marche as 'marche', r.lib_rubrique as 'rubrique', 
+        sb.lib_sous_rubrique as 'sous_rubrique', p.date_attachement as 'periode', 
+        ((c.valeur_constat * 100 ) / sb.valeur_cible) as 'taux_avancement_sous_rubrique' 
+        FROM periodes p inner join constats c on p.id_periode = c.id_periode 
+        inner join sous_rubriques sb on c.id_sous_rubrique = sb.id_sous_rubrique 
+        inner join rubriques r on r.id = sb.id_rubrique 
         inner join marches m on m.id_marche = r.id_marche");
     }
 
